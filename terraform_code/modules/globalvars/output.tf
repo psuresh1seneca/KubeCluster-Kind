2fd590ecf475280bf65daa450682267292c28801
psuresh1@myseneca.ca <psuresh1@mysenceca.ca>
2022-06-02 08:36:26 +0000

    terrform-code : output file created

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,14 @@
+
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Praveen"
+    "App"   = "Web"
+    "Project" = "Assignment1"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "Assignment1"
+}
